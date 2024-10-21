0ef8722d383f05c132a0db9f107cc6cdf94d0c7e
Kosto1221 <ostolany@gamil.com>
2024-10-21 16:02:36 +0900

    BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+import Router from "./Router";
+import React from "react";
 
 function App() {
   return (
-    <Container>
-      <H1>protected</H1>
-    </Container>
+    <div>
+      <Router />
+    </div>
   );
 }
 
